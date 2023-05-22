1d20d8fe77eae0d3f6c2f59d98f2cadd9afee85a
Christa Favano <crstav@gmail.com>
2023-05-22 19:01:56 -0400

    Added css changes, and game logic for storing input and updating display

--- script.js
+++ script.js
@@ -6,40 +6,90 @@ const playerFactory = (name, sign) => {
     }
 }
 
+const player1 = playerFactory("christa","x");
+const player2 = playerFactory("jordan","o");
+
+
+const gameManager = (() => {
+    let _board = [[],[],[]];
+    let isTurn;
+
+    let getPlayer = ( ) => {
+        if(isTurn == player2  || isTurn == null){
+            return isTurn = player1;
+           
+        }
+        else {
+          return   isTurn = player2;
+           // return player2;
+        }
+        
+    }
+    let boardUpdate = (obj) => {
+        _board[obj.arr[0]][obj.arr[1]] = player1.sign;
+        console.log(_board)
+        console.log('isWin?')
+    }
+    return {
+       boardUpdate,
+       getPlayer,
+       isTurn
+    }
+})()
+
+const isWin = ((player, board)=>{
+    const winningCombinations = [
+        [00,01,02],
+        [10,11,12],
+        [20,21,22],
+        [00,11,22], //diagonal
+        [02,11,20], //diagonal
+        [00,10,20], //vertical
+        [01,11,21], //vertical
+        [02,21,12]  //vertical
+    ]
+})()
+
 const gameBoard = (() => {
  
      let storeCell  = (obj) => {
         console.log(obj)
-        if(obj != null){
-           
-            console.log(obj);
+     
+        if(obj.arr != ''  ){
             displayController.updateDisplay(obj.event);
-           
-             return gameManager.boardUpdate(obj)
+            return gameManager.boardUpdate(obj)
         }
 
         else{
-             console.log('error')
-             return null;
+            console.log('error')
         }
     }
  
+    let checkCell = (e) => {
+        const divID = document.getElementById(e.target.id);
+        
+        if(divID.innerHTML != "") {
+            return false
+        }
+        
+        return true
+    }
+
     let getCell = (e) => {
-       // const divID = document.getElementById(e.target.id);
-        console.log(e.target.id)
-        const x = `${e.target.id.substring(0,1)}`;
-        console.log(x)
+        const x = e.target.id.substring(0,1);
         const y = e.target.id.substring(1);
         const obj = {arr:[x,y], event:e }
-        console.log(obj)
-      
-             gameBoard.storeCell(obj)
+        const cellStatus = checkCell(e);
         
+        if(cellStatus) {
+            return gameBoard.storeCell(obj)
+        }
+        
+        console.log('error')  
     }
         
     
     return {
-       // enterMove,
         storeCell, 
         getCell
     }
@@ -49,49 +99,28 @@ const gameBoard = (() => {
 const displayController = (() => {
   
     const spots = document.getElementsByClassName('box');
-let result;
-  //  const spots = document.getElementsByClassName('box');
-        for (let i = 0; i < spots.length; i++){
-            spots[i].addEventListener("click", (e) => {
-                  result = gameBoard.getCell(e);
-            })
-        }
-//add player
-    const updateDisplay = (e) => {
 
+    for (let i = 0; i < spots.length; i++){
+        spots[i].addEventListener("click", (e) => {
+              gameBoard.getCell(e);
+        })
+    }
+
+    const updateDisplay = (e) => {
+    
+        const player = gameManager.getPlayer();
         const divID = document.getElementById(e.target.id);
-        const textNode = document.createTextNode(player1['sign']);
+        const textNode = document.createTextNode(player['sign']);
         divID.appendChild(textNode);
     }
 
     return{
-       // array,
-     //  addListener,
        updateDisplay,
-       result
-       
     }
+
 })()
 
 
-const gameManager = (() => {
-    let board = [[],[],[]];
-    
-        console.log(displayController.result);
-      
-    
-    let boardUpdate = (obj) => {
-        board[obj.arr[0]][obj.arr[1]] = player1.sign;
-        console.log(board)
-    }
-
-    return {
-        //publicMethod
-        boardUpdate
- }
-  
-})()
 
 
-const player1 = playerFactory("christa","x");
 
